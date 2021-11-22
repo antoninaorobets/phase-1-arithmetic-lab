3a1dacde676b2dc7ebee5649465206c94cbe7e86
antoninaorobets <pani.antonina.don@gmail.com>
2021-11-22 15:41:29 -0500

    commit tree af405903bf672389c370291fe86c56054d24594d

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 2;
+const num2 = 31
 
+const  multiply = num1 * num2;
 // Write your code here
+
+const random =  Math.ceil(Math.random())
+
+
+
+const num3 = 19;
+const num4 = 5;
+const mod = num3 % num4
+
+const max = Math.max(2,20,12,3,4,6,7,9)
